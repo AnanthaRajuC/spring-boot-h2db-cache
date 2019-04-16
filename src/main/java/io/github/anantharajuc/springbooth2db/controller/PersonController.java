3773dc1fb81f0cb7e5530b479ecb479a064e4f58
Anantha Raju C <anantharaju.c@customercentria.com>
2019-04-16 14:32:14 +0530

    ADD Update url

--- src/main/java/io/github/anantharajuc/springbooth2db/controller/PersonController.java
+++ src/main/java/io/github/anantharajuc/springbooth2db/controller/PersonController.java
@@ -1,6 +1,7 @@
 package io.github.anantharajuc.springbooth2db.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.anantharajuc.springbooth2db.model.Person;
+import io.github.anantharajuc.springbooth2db.repository.PersonRepository;
 import io.github.anantharajuc.springbooth2db.service.PersonServiceImpl;
 
 @RestController
@@ -23,6 +26,9 @@ public class PersonController
 {
 	@Autowired
 	PersonServiceImpl personServiceImpl;
+	
+	@Autowired
+	PersonRepository personRepository;
 	
 	/**
      * Retrieve all persons
@@ -63,6 +69,30 @@ public class PersonController
         personServiceImpl.saveOrUpdate(person);
         return person; 
     }
+	
+	/**
+     * Update a person. Hero must exist for id.
+     * 
+     * @param id. The id of the person to be updated
+     * @param person. The person values to be updated
+     * @throws ResourceNotFoundException if person not found.
+     */
+	@PutMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	private void updatePerson(@PathVariable("id") Long id, @RequestBody Person person)
+	{
+		Optional<Person> updateablePerson = personRepository.findById(id); 
+		
+		if(updateablePerson.isPresent())
+		{
+			
+			personRepository.save(person);
+		}
+		else 
+		{
+			throw new ResourceNotFoundException("Person not found");
+		}		
+	}
 	
 	/**
      * Delete Person
